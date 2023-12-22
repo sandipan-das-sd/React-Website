7cc3869b0a51e2a81f0066c3f94c4e39729aab36
Sandipan Das <sandipanpyqplatform@gmail.com>
2023-12-22 10:36:28 +0530

    React website

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Header = () =>{
+    return(
+        <div className="header">
+          <div className="mobile">
+            <a href="tel:+918335019404"><i class="bi bi-telephone-outbound"></i> + 91-8335019404</a>
+        
+          </div>
+        </div>
+    )
+
+}
+export default Header;
